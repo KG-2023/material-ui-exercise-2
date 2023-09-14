ae81804a0951da26c21be5ae30d67ccd8ed1f053
Rodrigo Lopez <scdtp2019@gmail.com>
2023-09-14 20:57:01 +0800

    Add Typography variant

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import TourCard from './components/TourCard';
--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -2,13 +2,22 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 
 
 const TourCard = () =>{
   return (
     <Grid item xs={3}>
-        <Box />
+       
         <Paper elevation={10}  square> TEST HELLO WORLD! </Paper>
+        <Paper elevation={10}  square> 
+          <img src="https://raw.githubusercontent.com/KG-2023/Activity-Images-2023/ec633589ea59e21cc1d3aca3e84cc9ab7a7ce0ba/pexels-balaji-srinivasan-3936815.jpg" className="paperImage" />
+          
+          <Typography variant="h2" component="h2">
+            Tabung Hampi
+          </Typography>
+
+        </Paper>
     </Grid>
     
   )
