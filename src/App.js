cc61a3563cd740dca53afaeeba33357faab61438
Rodrigo Lopez <scdtp2019@gmail.com>
2023-09-15 20:37:54 +0800

    Add grid container and set TourCard as a prop

--- src/App.js
+++ src/App.js
@@ -4,19 +4,39 @@ import AppBar from './components/AppBar'
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import TourCard from './components/TourCard';
+import {Typography} from '@mui/material/'
+import CitiesToTravel from './data.json';
 
 function App() {
   return (
     <div className="App">
        <AppBar />
       <Container sx={{marginY:5}}>
-       
-        <Grid container spacing={5}>
+        {CitiesToTravel.map((city) =>(
+          <>
+            <Typography 
+             variant="h4"
+             component="h2"
+             marginTop={5}
+              marginBottom={3}
+            >Top {city.name} Tours</Typography>
+            <Grid container spacing={5}>
+              {city.tours.map((tour) => <TourCard tour={tour}/>)}
+            </Grid>
+          </>
+          
+        )
+        
+        )}
+        
+      
+
+        {/* <Grid container spacing={5}>
         <TourCard />
         <TourCard />
         <TourCard />
         <TourCard />
-        </Grid>
+        </Grid> */}
       </Container>
       
     </div>
