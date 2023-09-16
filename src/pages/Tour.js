70f3672a33c1e0fc19d3bbada3d60258d8f00c5e
Rodrigo Lopez <scdtp2019@gmail.com>
2023-09-16 09:51:22 +0800

    Add ImageLists component

--- src/pages/Tour.js
+++ src/pages/Tour.js
@@ -1,5 +1,20 @@
+import {Container} from '@mui/material'
+import { Typography } from '@mui/material/';
+import Box from '@mui/material/Box';
+import ImageItems from "../components/ImageItems";
 const Tour = () => {
-    return <div>Test TOUR PATH</div>
+    return <Container>
+            <Typography variant="h3" component="h1" marginTop={3}>
+                Explore the Rice Terraces
+
+            </Typography>
+            <Box marginTop={3} 
+                sx={{display: "flex"}}
+            >
+                <ImageItems />  
+            </Box>
+        </Container>
+    
 }
 
 export default Tour;
